899776aaa91c083eec36300efdaa3c1d3d6533a4
Daniel Hillier <daniel.hillier@gmail.com>
2020-08-05 15:56:07 +1000

    Show crash when return type of a Manager method is a forward reference

--- djmypytest/test_app/models.py
+++ djmypytest/test_app/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class ManagerA(models.Manager):
 
-    def do_something(self, other_obj: "ModelB"):
-        return None
+    def do_something(self) -> "ModelB":
+        return ModelB.objects.create(movie="There's something about mypy")
 
 
 class ModelA(models.Model):
